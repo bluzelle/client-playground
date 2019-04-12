e792a2af7ecfb881a9a12ab634c5f9dcdd310a27
Yarco Hayduk <yarcoh@gmail.com>
2019-04-12 16:13:48 -0700

    changed the py runtime file

--- py_run.sh
+++ py_run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python3 src/py/cpp_connect.py
+#python3 src/py/cpp_connect.py
+python3 src/py/cpp_ptr.py
--- src/py/cpp_ptr.py
+++ src/py/cpp_ptr.py
@@ -1,5 +1,4 @@
 # https://github.com/aio-libs - more libs here
-from build import libdb
 import sys
 import os
 import asyncio
@@ -12,7 +11,7 @@ uvloop.install()
 
 sys.path.extend([os.getcwd()])
 
-
+from build import libdb
 
 # print("Own?", response_ptr.thisown)
 # response_ptr.thisown = True
@@ -23,9 +22,9 @@ db = libdb.DB()
 print(db.getResponseSharedPtrCount()) # 1
 response_ptr = db.getResponseSharedPtr()
 print(db.getResponseSharedPtrCount()) # 2
-object_methods = [method_name for method_name in dir(response_ptr)
-                  if callable(getattr(response_ptr, method_name))]
-print(object_methods)
+# object_methods = [method_name for method_name in dir(response_ptr)
+#                   if callable(getattr(response_ptr, method_name))]
+# print(object_methods)
 response_ptr = None
-print(db.getResponseSharedPtrCount()) # 1
+print(db.getResponseSharedPtrCount()) # SHOULD BE 1
 
